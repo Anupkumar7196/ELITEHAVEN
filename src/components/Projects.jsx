7ac474cb74d9704d4e1d51d4db0972d171964123
Anup kumar <anupk7196@gmail.com>
2025-07-03 02:27:20 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { assets, projectsData } from '../assets/assets'
-
+import { motion } from 'framer-motion'
 const Projects = () => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -32,7 +32,12 @@ const Projects = () => {
     }
 
   return (
-    <div className='container mx-auto py-4 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+    <motion.div 
+    initial={{opacity: 0, x: -200}}
+      transition={{ duration: 1 }}
+      whileInView={{opacity: 1, x: 0}}
+      viewport={{ once: true }}
+    className='container mx-auto py-4 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explore Our Protfolio</p>
       {/* Slider button */}
@@ -60,7 +65,7 @@ const Projects = () => {
             ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
